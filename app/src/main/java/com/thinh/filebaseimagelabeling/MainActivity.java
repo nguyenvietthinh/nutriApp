2001d9258ae7c172b9fba2d6816d8bc60b6f5b39
nvietthinh <vthinh741@gmail.com>
2019-08-29 15:44:25 +0700

    1st

---
+++ app/src/main/java/com/thinh/filebaseimagelabeling/MainActivity.java
@@ -0,0 +1,158 @@
+package com.thinh.filebaseimagelabeling;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionCloudImageLabelerOptions;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.google.firebase.ml.vision.label.FirebaseVisionOnDeviceImageLabelerOptions;
+import com.thinh.filebaseimagelabeling.helper.InternetCheck;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    CameraView cameraView;
+    Button btnDetect;
+    AlertDialog waitingDialog;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.stop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraView = (CameraView) findViewById(R.id.camemraView);
+        btnDetect = (Button)findViewById(R.id.btnDetect);
+
+        waitingDialog = new SpotsDialog.Builder().setContext(this).setMessage("Please waiting...").setCancelable(false).build();
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                waitingDialog.show();
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap =  Bitmap.createScaledBitmap(bitmap,bitmap.getWidth(),bitmap.getHeight(),false);
+                cameraView.stop();
+
+                runDetect(bitmap);
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+
+        btnDetect.setOnClickListener((view)->{
+            cameraView.start();
+            cameraView.captureImage();
+        });
+    }
+
+    private void runDetect(Bitmap bitmap) {
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        new InternetCheck(new InternetCheck.Consumer() {
+            @Override
+            public void accept(boolean internet) {
+                if(internet){
+                    FirebaseVisionCloudImageLabelerOptions options =
+                         new FirebaseVisionCloudImageLabelerOptions.Builder()
+                             .setConfidenceThreshold(0.8f)
+                             .build();
+                    FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                         .getCloudImageLabeler(options);
+                    labeler.processImage(image)
+                            .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                                @Override
+                                public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                                    processDataResult(labels);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("EDMTERROR",e.getMessage());
+                                }
+                            });
+                }else {
+                    FirebaseVisionOnDeviceImageLabelerOptions options =
+                         new FirebaseVisionOnDeviceImageLabelerOptions.Builder()
+                             .setConfidenceThreshold(0.8f)
+                             .build();
+                    FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                         .getOnDeviceImageLabeler(options);
+                    labeler.processImage(image)
+                            .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                                @Override
+                                public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                                    processDataResult(labels);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d("EDMTERROR",e.getMessage());
+                                }
+                            });
+                }
+            }
+        });
+
+    }
+
+    private void processDataResult(List<FirebaseVisionImageLabel> labels) {
+        for (FirebaseVisionImageLabel label : labels){
+            String text = label.getText();
+
+            Toast.makeText(this,"Result: "+text,Toast.LENGTH_LONG).show();
+        }
+        if (waitingDialog.isShowing()){
+            waitingDialog.dismiss();
+        }
+    }
+}
