f9756168bdd0e80010c3eeaa4c5965fd12b2b099
nvietthinh <vthinh741@gmail.com>
2019-08-30 15:36:59 +0700

    2st

--- app/src/main/java/com/thinh/filebaseimagelabeling/MainActivity.java
+++ app/src/main/java/com/thinh/filebaseimagelabeling/MainActivity.java
@@ -1,8 +1,5 @@
 package com.thinh.filebaseimagelabeling;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -10,15 +7,18 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.label.FirebaseVisionCloudImageLabelerOptions;
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
 import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
 import com.google.firebase.ml.vision.label.FirebaseVisionOnDeviceImageLabelerOptions;
+import com.thinh.filebaseimagelabeling.enums.ImageDetectEngine;
 import com.thinh.filebaseimagelabeling.helper.InternetCheck;
 import com.wonderkiln.camerakit.CameraKitError;
 import com.wonderkiln.camerakit.CameraKitEvent;
@@ -53,10 +53,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         cameraView = (CameraView) findViewById(R.id.camemraView);
         btnDetect = (Button)findViewById(R.id.btnDetect);
-
         waitingDialog = new SpotsDialog.Builder().setContext(this).setMessage("Please waiting...").setCancelable(false).build();
 
         cameraView.addCameraKitListener(new CameraKitEventListener() {
@@ -64,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
             public void onEvent(CameraKitEvent cameraKitEvent) {
 
             }
-
             @Override
             public void onError(CameraKitError cameraKitError) {
 
@@ -72,11 +69,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onImage(CameraKitImage cameraKitImage) {
+
                 waitingDialog.show();
                 Bitmap bitmap = cameraKitImage.getBitmap();
                 bitmap =  Bitmap.createScaledBitmap(bitmap,bitmap.getWidth(),bitmap.getHeight(),false);
                 cameraView.stop();
-
                 runDetect(bitmap);
             }
 
@@ -92,64 +89,59 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Get the vision image detecter then analyze the image bitmap.
+     * @param bitmap the picture taken from camera.
+     */
     private void runDetect(Bitmap bitmap) {
 
+        //Create a FirebaseVisionImage object from a Bitmap object
         FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
 
-        new InternetCheck(new InternetCheck.Consumer() {
-            @Override
-            public void accept(boolean internet) {
-                if(internet){
-                    FirebaseVisionCloudImageLabelerOptions options =
-                         new FirebaseVisionCloudImageLabelerOptions.Builder()
-                             .setConfidenceThreshold(0.8f)
-                             .build();
-                    FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
-                         .getCloudImageLabeler(options);
-                    labeler.processImage(image)
-                            .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
-                                @Override
-                                public void onSuccess(List<FirebaseVisionImageLabel> labels) {
-                                    processDataResult(labels);
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.d("EDMTERROR",e.getMessage());
-                                }
-                            });
-                }else {
-                    FirebaseVisionOnDeviceImageLabelerOptions options =
-                         new FirebaseVisionOnDeviceImageLabelerOptions.Builder()
-                             .setConfidenceThreshold(0.8f)
-                             .build();
-                    FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
-                         .getOnDeviceImageLabeler(options);
-                    labeler.processImage(image)
-                            .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
-                                @Override
-                                public void onSuccess(List<FirebaseVisionImageLabel> labels) {
-                                    processDataResult(labels);
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.d("EDMTERROR",e.getMessage());
-                                }
-                            });
-                }
-            }
+        new InternetCheck(internet -> {
+            FirebaseVisionImageLabeler detectImageLabeler = getFirebaseVisionImageLabeler(internet ? ImageDetectEngine.COULD_ENGINE : ImageDetectEngine.DEVICE_ENGINE);
+            detectImageLabeler.processImage(image)
+                    .addOnSuccessListener(labels -> processDataResult(labels))
+                    .addOnFailureListener(e -> Log.d("EDMTERROR",e.getMessage()));
         });
 
     }
 
+    /**
+     * Get image labeler after validate
+     * @param imageDetectEngine types of image labeler
+     */
+    private FirebaseVisionImageLabeler getFirebaseVisionImageLabeler(ImageDetectEngine imageDetectEngine) {
+        if(imageDetectEngine == null)
+            throw new IllegalArgumentException("Engine must be specified");
+        switch (imageDetectEngine)
+        {
+            case COULD_ENGINE:
+                FirebaseVisionCloudImageLabelerOptions cloudImageLabelerOptions =
+                        new FirebaseVisionCloudImageLabelerOptions.Builder()
+                                .setConfidenceThreshold(0.8f) // Get highest Confidence Threshold
+                                .build();
+                return FirebaseVision.getInstance()
+                        .getCloudImageLabeler(cloudImageLabelerOptions);
+            case DEVICE_ENGINE:
+                FirebaseVisionOnDeviceImageLabelerOptions deviceImageLabelerOptions =
+                        new FirebaseVisionOnDeviceImageLabelerOptions.Builder()
+                                .setConfidenceThreshold(0.8f) // Get highest Confidence Threshold
+                                .build();
+                return FirebaseVision.getInstance()
+                        .getOnDeviceImageLabeler(deviceImageLabelerOptions);
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Get the label's text description then the image labeling operation succeeds
+     * @param labels a list of FirebaseVisionImageLabel objects after detect image
+     */
     private void processDataResult(List<FirebaseVisionImageLabel> labels) {
         for (FirebaseVisionImageLabel label : labels){
-            String text = label.getText();
-
-            Toast.makeText(this,"Result: "+text,Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"Result: "+label.getText(),Toast.LENGTH_LONG).show();
         }
         if (waitingDialog.isShowing()){
             waitingDialog.dismiss();
